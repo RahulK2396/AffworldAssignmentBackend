51383387778e54d3d81e39f36fb3a52919930305
RahulK2396 <121452019+RahulK2396@users.noreply.github.com>
2025-01-26 11:27:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ const corsOptions = {
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 };
-
+app.options('*', cors(corsOptions));
 app.use(cors(corsOptions));
-app.options('*', cors(corsOptions)); 
+ 
 app.use(express.json());
 app.use(
   session({
